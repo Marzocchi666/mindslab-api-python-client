21c937ea37ad46f7d8ea5cc6c19848b940491a16
herbertml <herbert@mindslab.ai>
2017-11-14 11:48:38 +0900

    move to mindslab/ and add test case

--- mindslab/client.py
+++ mindslab/client.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 class MindsAPIClient(object):
-    ''' Minds API client container.
+    ''' default Minds API client.
     '''
 
-    __slots__ = [
-        '__version'
-        'account_id',
-        'account_key',
-        'endpoint',
-        'tag',
-    ]
+    __slots__ = ['__version__',
+                 'account_id',
+                 'account_key',
+                 'endpoint',
+                 'tag']
 
     def __init__(self,
                  account_id=None,
@@ -21,43 +18,47 @@ class MindsAPIClient(object):
         self.endpoint = 'https://mindsapi.mindslab.ai/api/'
 
         # credentials
-        self.account_id = account_id
-        self.account_key = account_key
+        self.account_id = str(account_id)
+        self.account_key = str(account_key)
 
         # tag
         import uuid # lazy load
         self.tag = tuple(str(uuid.uuid4()))
 
         # version
-        self.__version = '1.0.0-beta'
+        self.__version__ = '1.0.1'
 
-    def __version__(self):
-        return self.__version__
+    def __eq__(self, __object__):
+        return isinstance(__object__, self.__class__)
 
     def get_endpoint(self):
+        ''' get API endpoint.
+        '''
         return self.endpoint
 
     def get_account_id(self):
-        ''' return Account ID.
+        ''' get account ID (token).
         '''
         return self.account_id
 
     def get_account_key(self):
-        ''' return Account Key.
+        ''' get account key.
         '''
         return self.account_key
 
     def get_tag(self):
+        ''' get client tag.
+        '''
         return self.tag
 
     def set_account_id(self, account_id):
-        ''' set Account ID.
+        ''' set account ID (token).
         '''
         if not account_id: return
         self.account_id = str(account_id)
 
     def set_account_key(self, account_key):
-        ''' set Account Key.
+        ''' set account key.
         '''
         if not account_key: return
-        self.account_key = account_key
+        self.account_key = str(account_key)
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python
 
-from src.client import MindsAPIClient
+from mindslab.client import MindsAPIClient
 
 import unittest
+import sys
 
 class TestMindsAPIClient(unittest.TestCase):
     ''' unit test for Minds API client container.
     '''
 
+    client = MindsAPIClient()
+
     def test_client_initialization(self):
-        client = MindsAPIClient()
-        self.assertTrue(client != None)
-        self.assertTrue(isinstance(client, MindsAPIClient))
+        self.assertFalse(self.client.__eq__(None))
+        self.assertTrue(isinstance(self.client, MindsAPIClient))
+
+    def test_client_endpoint(self):
+        self.assertTrue(self.client.get_endpoint().__eq__('https://mindsapi.mindslab.ai/api/'))
+
+    def test_client_tag(self):
+        self.assertTrue(bool(self.client.get_tag()))
+
+if __name__ == '__main__':
+    test_suite = unittest.TestLoader().loadTestsFromTestCase(TestMindsAPIClient)
+    result = unittest.TextTestRunner(verbosity=2).run(unittest.TestSuite(test_suite))
+    sys.exit(not result.wasSuccessful())
