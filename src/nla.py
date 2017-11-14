006773ba74ca0a6677db1fc19760fcc84e91c81f
herbertml <herbert@mindslab.ai>
2017-11-14 10:41:41 +0900

    initial commit

---
+++ src/nla.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .client import MindsAPIClient
+
+try: import requests
+except ImportError as error: raise error
+
+import json
+import os
+import sys
+
+NLA_STATUS_OK = 'Success'
+NLA_STATUS_FAIL = 'Fail'
+
+class MindsNLAClient(MindsAPIClient):
+    ''' Minds API NLA client.
+    '''
+
+    def __init__(self,
+                 lang='kr',
+                 level=None,
+                 keyword='8000'):
+
+        # load MindsAPIClient
+        super(MindsAPIClient, self).__init__()
+
+        # endpoint
+        self.endpoint += 'nla/'
+
+        # nla model
+        self.level = level
+        self.keyword_level = keyword_level
+
+        # metadata
+        self.__version = '0.1.0'
+
+    #
+    # public
+    #
+
+    def get_model(self):
+        '''
+        '''
+        return dict([
+            ('level', self.level),
+            ('keyword_level', self.keyword_level)
+        ])
+
+    def set_model(self,
+                   level,
+                   keyword_level):
+        '''
+        '''
+        self.level = level
+        self.keyword_level = keyword_level
+
+    def get_models(self, verbose=False):
+        ''' retrieve available NLA models from server.
+        '''
+        data = {
+            'cmd': 'getNLAModels',
+            'ID': self.account_id,
+            'key': self.account_key
+        }
+        with requests.post(
+                self.endpoint,
+                files={},
+                data=data
+        ) as res:
+            if res.status_code != 200: return
+            try:
+                payload = json.loads(res.text)
+                status = payload.get('status')
+            except: return
+            if status == NLA_STATUS_OK:
+                nla_models = json.loads(payload.get('data'))
+            else:
+                nla_models = payloads.get('data')
+            if verbose: sys.stdout.write('%s\n' % nla_models)
+            return (nla_models, status)
+
+    def get_nla_result(self,
+                       sentence,
+                       verbose=False):
+        '''
+        '''
+        data = {
+            'cmd': 'runNLA',
+            'ID': self.account_id,
+            'key': self.account_key,
+            'level': self.level,
+            'keyword_level': self.keyword_level,
+            'sentence': str(sentence)
+        }
+        with requests.post(self.endpoint, data=data) as res:
+            if res.status_code != 200: return
+            try:
+                payload = json.loads(res.text)
+                status = payload.get('status')
+                data = payload.get('data')
+            except: return
+        if verbose: sys.stdout.write('%s:%s\n' % (status, data))
+        return (status, data)
